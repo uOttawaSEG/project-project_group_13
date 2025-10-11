5b1312f504b7bcd29d8af36f39b504726a734658
DouaaAchiai <dachi040@uottawa.ca>
2025-10-11 18:13:16 -0400

    Add (app): user class (#28)

---
+++ OTAMS/app/src/main/java/com/example/otams/User.java
@@ -0,0 +1,32 @@
+public class User {
+    protected String username;
+    protected String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
+
+class Admin extends User {
+
+    public Admin(String username, String password) {
+        super(username, password);
+    }
+
+    public void accept() {
+        System.out.println("Admin " + username + " a accepté la demande.");
+    }
+
+    public void reject() {
+        System.out.println("Admin " + username + " a rejeté la demande.");
+    }
+}
