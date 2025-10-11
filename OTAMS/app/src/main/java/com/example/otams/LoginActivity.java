e28b29f74a2b6770fedc524dc9dcf2c7b4b8dd25
Mani <mani.masteali7@gmail.com>
2025-10-11 18:38:17 -0400

    created a prototype of the login page

--- OTAMS/app/src/main/java/com/example/otams/LoginActivity.java
+++ OTAMS/app/src/main/java/com/example/otams/LoginActivity.java
@@ -15,7 +15,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginSpinner), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
