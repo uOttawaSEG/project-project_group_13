ce4bd72054a934d5fc0d2f6773dd32d41e536d79
Joe <jthoz081@uottawa.ca>
2025-10-12 17:20:47 -0400

    login fragment created

--- OTAMS/app/src/main/java/com/example/otams/Client.java
+++ OTAMS/app/src/main/java/com/example/otams/Client.java
@@ -1,3 +1,5 @@
+package com.example.otams;
+
 public class Client extends User {
     private String First_Name;
     private String Last_Name;
@@ -15,20 +17,45 @@ public class Client extends User {
         this.Past_Session = Past_Session;
     }
 
-    public String getFirst_Name() { return First_Name; }
-    public void setFirst_Name(String First_Name) { this.First_Name = First_Name; }
+    public String getFirst_Name() {
+        return First_Name;
+    }
 
-    public String getLast_Name() { return Last_Name; }
-    public void setLast_Name(String Last_Name) { this.Last_Name = Last_Name; }
+    public void setFirst_Name(String First_Name) {
+        this.First_Name = First_Name;
+    }
 
-    public String getPhone_Number() { return Phone_Number; }
-    public void setPhone_Number(String Phone_Number) { this.Phone_Number = Phone_Number; }
+    public String getLast_Name() {
+        return Last_Name;
+    }
 
-    public String getFuture_Session() { return Future_Session; }
-    public void setFuture_Session(String Future_Session) { this.Future_Session = Future_Session; }
+    public void setLast_Name(String Last_Name) {
+        this.Last_Name = Last_Name;
+    }
 
-    public String getPast_Session() { return Past_Session; }
-    public void setPast_Session(String Past_Session) { this.Past_Session = Past_Session; }
+    public String getPhone_Number() {
+        return Phone_Number;
+    }
+
+    public void setPhone_Number(String Phone_Number) {
+        this.Phone_Number = Phone_Number;
+    }
+
+    public String getFuture_Session() {
+        return Future_Session;
+    }
+
+    public void setFuture_Session(String Future_Session) {
+        this.Future_Session = Future_Session;
+    }
+
+    public String getPast_Session() {
+        return Past_Session;
+    }
+
+    public void setPast_Session(String Past_Session) {
+        this.Past_Session = Past_Session;
+    }
 
     public void accept() {
         System.out.println("Session accepted.");
