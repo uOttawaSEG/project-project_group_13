857e763a3c8b8f1f0c7f878bde67e6504b1a0c97
Joe <jthoz081@uottawa.ca>
2025-10-12 17:47:39 -0400

    remove problem xml

--- OTAMS/app/src/main/java/com/example/otams/LoginActivity.java
+++ OTAMS/app/src/main/java/com/example/otams/LoginActivity.java
@@ -14,7 +14,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.fragment_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginSpinner), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
