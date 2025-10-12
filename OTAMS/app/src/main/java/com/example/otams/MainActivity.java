6845d1b09a6544ce22aa422dfc37fec4a97565d4
Joe Thozhuthumparambil <39406155+Forever-Uselesss@users.noreply.github.com>
2025-10-12 18:09:54 -0400

    Fix for bulild (#34)

--- OTAMS/app/src/main/java/com/example/otams/MainActivity.java
+++ OTAMS/app/src/main/java/com/example/otams/MainActivity.java
@@ -8,14 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.otams.databinding.FragmentLoginBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private FragmentLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginSpinner), (v, insets) -> {
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainContainer), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
