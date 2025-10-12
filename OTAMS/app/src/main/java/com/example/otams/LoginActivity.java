a0af6d9cbb0ed50c6e091490d4a4c93898fc1d9c
Joe Thozhuthumparambil <39406155+Forever-Uselesss@users.noreply.github.com>
2025-10-12 18:15:02 -0400

    fix (otams): Bug fix for login (#35)

--- OTAMS/app/src/main/java/com/example/otams/LoginActivity.java
+++ OTAMS/app/src/main/java/com/example/otams/LoginActivity.java
@@ -15,7 +15,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.fragment_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginSpinner), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainContainer), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
