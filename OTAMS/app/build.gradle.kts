4ca633677321b43e3192dcf3fe7db659e5d57efa
Joe <jthoz081@uottawa.ca>
2025-10-12 17:38:11 -0400

    added navigation components and rebased ui branch

--- OTAMS/app/build.gradle.kts
+++ OTAMS/app/build.gradle.kts
@@ -43,6 +43,8 @@ dependencies {
     implementation(libs.annotation)
     implementation(libs.lifecycle.livedata.ktx)
     implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
