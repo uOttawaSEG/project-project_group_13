f469259c4ca293308eb7dc03e9623b3f0abe7ecb
Mani <mani.masteali7@gmail.com>
2025-10-13 16:53:37 -0400

    some of the widgets will now hide based on the role the user selects

--- OTAMS/app/src/main/java/com/example/otams/RegisterActivity.java
+++ OTAMS/app/src/main/java/com/example/otams/RegisterActivity.java
@@ -2,6 +2,10 @@ package com.example.otams;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,5 +29,33 @@ public class RegisterActivity extends AppCompatActivity {
             Intent intent =  new Intent(RegisterActivity.this,LoginActivity.class);
             startActivity(intent);
         });
-    }
+        // the spinner will hide the widgets program of study, highest degree and courses offered and only display them when the user toggles the role
+        Spinner rolespinner = findViewById(R.id.registerSpinner);
+        EditText programOfStudyEditText = findViewById(R.id.programOfStudyEditText);
+        EditText highestDegreeEditText = findViewById(R.id.highestDegreeEditText);
+        EditText coursesOfferedEditText = findViewById(R.id.coursesOfferedEditText);
+        programOfStudyEditText.setVisibility(View.GONE);
+        highestDegreeEditText.setVisibility(View.GONE);
+        coursesOfferedEditText.setVisibility(View.GONE);
+        rolespinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedROLE = parent.getItemAtPosition(position).toString();
+                if (selectedROLE.equals("Student")) {
+                    programOfStudyEditText.setVisibility(View.VISIBLE);
+                    highestDegreeEditText.setVisibility(View.GONE);
+                    coursesOfferedEditText.setVisibility(View.GONE);
+                } else if (selectedROLE.equals("Tutor")) {
+                    programOfStudyEditText.setVisibility(View.GONE);
+                    highestDegreeEditText.setVisibility(View.VISIBLE);
+                    coursesOfferedEditText.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // nothing
+            }
+        })
+    ;}
 }
