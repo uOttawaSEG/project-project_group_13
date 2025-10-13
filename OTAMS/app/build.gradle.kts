318dff0b7e234cfbd781d871c6d9368a4c40aac9
Joe <jthoz081@uottawa.ca>
2025-10-13 16:49:56 -0400

    feat (app): config firebase

--- OTAMS/app/build.gradle.kts
+++ OTAMS/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -47,6 +48,7 @@ dependencies {
     implementation(libs.navigation.ui)
     implementation(libs.fragment)
     implementation(libs.recyclerview)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- OTAMS/app/src/main/java/com/example/otams/ui/login/fragment_signup.java
+++ OTAMS/app/src/main/java/com/example/otams/ui/login/fragment_signup.java
@@ -1,4 +1,4 @@
-package com.example.otams;
+package com.example.otams.ui.login;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.otams.R;
 import com.google.android.material.appbar.MaterialToolbar;
 
 public class fragment_signup extends Fragment {
