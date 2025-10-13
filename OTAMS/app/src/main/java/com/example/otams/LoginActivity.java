abce7856901653ab7efcc3690051e2adb010a947
Joe <jthoz081@uottawa.ca>
2025-10-12 20:02:47 -0400

    Update (resources): Uploaded relevant documents

--- OTAMS/app/src/main/java/com/example/otams/LoginActivity.java
+++ OTAMS/app/src/main/java/com/example/otams/LoginActivity.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends MainActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
