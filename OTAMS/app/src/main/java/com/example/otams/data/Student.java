435d3179f35d195e006122b66b53b5da7dc5b1c4
Joe <jthoz081@uottawa.ca>
2025-11-10 02:02:34 -0500

    start

--- OTAMS/app/src/main/java/com/example/otams/data/Student.java
+++ OTAMS/app/src/main/java/com/example/otams/data/Student.java
@@ -2,27 +2,27 @@ package com.example.otams.data;
 
 public class Student extends Client {
 
-    private String Program_Of_Study;
+    private String program_of_study;
 
     public Student(String Email, String Password, String First_Name, String Last_Name, String Phone_Number,
-            String Future_Session, String Past_Session, String Program_Of_Study) {
+            String Future_Session, String Past_Session, String program_of_study) {
         super(Email, Password, First_Name, Last_Name, Phone_Number, Future_Session, Past_Session);
-        this.Program_Of_Study = Program_Of_Study;
+        this.program_of_study = program_of_study;
         setRole(UserRole.STUDENT);
     }
 
     public Student(String Email, String Password, String First_Name, String Last_Name, String Phone_Number,
-            String Program_Of_Study) {
+            String program_of_study) {
         super(Email, Password, First_Name, Last_Name, Phone_Number);
-        this.Program_Of_Study = Program_Of_Study;
+        this.program_of_study = program_of_study;
         setRole(UserRole.STUDENT);
     }
 
-    public String getProgram_Of_Study() {
-        return Program_Of_Study;
+    public String getProgram_of_study() {
+        return program_of_study;
     }
 
-    public void setProgram_Of_Study(String Program_Of_Study) {
-        this.Program_Of_Study = Program_Of_Study;
+    public void setProgram_of_study(String program_of_study) {
+        this.program_of_study = program_of_study;
     }
 }
